412aa8bdea7d4a3e67431b6ced1c0e857ce5b1c4
Henrykhael <henricksoares@outlook.com.br>
2024-09-11 07:34:09 -0300

    Primeiro commit da atividade do Hello World no Java

---
+++ src/main/java/br/com/phcs/firstclass/First-Class.java
@@ -0,0 +1,5 @@
+package br.com.phcs.firstclass;
+
+public class First-Class {
+    
+}
---
+++ src/test/java/br/com/phcs/firstclass/FirstclassApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.phcs.firstclass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstclassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
