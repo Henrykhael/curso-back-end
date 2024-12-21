cceafe38e5c0676050883f8b10c5a435efd485f6
Henrykhael <henricksoares@outlook.com.br>
2024-12-21 20:02:29 -0300

    Primeiro crud

---
+++ crud/src/cadastroCliente.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author henri
+ */
+public class cadastroCliente {
+    
+    public static void main(String[] args){
+        TelaPrincipal tela = new TelaPrincipal();
+        tela.setVisible(true);
+    }
+    
+}
